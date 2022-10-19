ec8f17bd3059092b86fd57ced4f1b19700f3547d
blvdes <jaykesberry@gmail.com>
2022-10-19 08:33:18 +0100

    Update url retrieval.py

--- url retrieval.py
+++ url retrieval.py
@@ -5,5 +5,5 @@ conf = tekore.config_from_file(file, return_refresh=True)
 token = tekore.refresh_user_token(*conf[:2], conf[3])
 spotify = tekore.Spotify(token)
 
-topTrack = spotify.current_user_top_tracks(time_range = 'short_term', limit=1)
-print(topTrack.items[0].album.images[0].url)
+topTrack = spotify.current_user_top_tracks(time_range = 'short_term', limit=2)
+print(topTrack.items[1].album.images[0].url)
